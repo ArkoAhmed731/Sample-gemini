9b88cb44eec6c8ea6984f0b38d44f907ae172c00
Tahseen Ahmed Bhuiyan <tahseen.bhuiyan.ta@gmail.com>
2024-05-11 02:42:11 +0600

    finally solved the problem

--- gemini_explorer.py
+++ gemini_explorer.py
@@ -3,3 +3,15 @@ import streamlit as st
 from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerationModel, Part, Content, ChatSession
 
+project = "sample-gemini"
+vertexai.init(project=project)
+
+config = generative_models.GenerationConfig(
+    temperature=0.4
+)
+
+model = GenerationModel(
+    "gemini-pro",
+    generation_config = config
+)
+chat = model.start_chat()
